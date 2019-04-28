22a2c95121ac2a7b0434b8400cd01ad59dd0d89c
Luis Montero <luismontero@LAAPJG5H355-LUIS.local>
2019-04-27 21:48:27 -0600

    Add getOne option

--- Path/index.js
+++ Path/index.js
@@ -1,25 +1,38 @@
-const { MongoClient } = require("mongodb");
-
+const { MongoClient, ObjectID } = require("mongodb");
+const helpers = require("../helpers");
 module.exports = async function(context, req) {
+  const { method, query } = req;
   context.log("JavaScript HTTP trigger function processed a request.");
-    try {
-      const client = await MongoClient.connect(process.env.DBURL, {
-        useNewUrlParser: true
-      });
-      const db = client.db();
-      const col = db.collection("paths");
-      const docs = await col
-        .find()
-        .toArray();
-      context.res = { 
-        body: docs,
-        headers: { "Content-Type": "application/json" }
-      };
-      
-      client.close();
-      context.done();
-    } catch (e) {
-      console.error(e);
-    }
+  try {
+    switch (method) {
+      case "POST":
+        console.log("test");
+        break;
+      default:
+        //GET
+        let searchQuery = {};
+        if (query.id) {
+          //validate id format
+          if (query.id.length > 23) {
+            searchQuery = { _id: ObjectID(query.id) };
+          } else {
+            searchQuery = { _id: "" };
+          }
+        }
+        const client = await MongoClient.connect(process.env.DBURL, {
+          useNewUrlParser: true
+        });
+        const db = client.db();
+        const col = db.collection("paths");
+        const docs = await col.find(searchQuery).toArray();
 
+        const response = docs.length == 1 ? docs[0] : docs;
+        helpers.toJson(context, response);
+        client.close();
+        context.done();
+        break;
+    }
+  } catch (e) {
+    console.error(e);
+  }
 };
